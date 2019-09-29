7728e04dcce6656dfaa975c70de6db795f5b3da9
Kate Akentyeva <katerina.akent@gmail.com>
2019-09-29 23:18:17 +0300

    Update MainWindow logic

--- ComputerGraphicsAlgorithms/MainWindow.xaml.cs
+++ ComputerGraphicsAlgorithms/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GraphicsServices;
+using GraphicsServices.RenderObjTypes;
+using System;
+using System.Numerics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Camera = GraphicsServices.RenderObjTypes.Camera;
 
 namespace ComputerGraphicsAlgorithms
 {
@@ -20,9 +14,63 @@ namespace ComputerGraphicsAlgorithms
     /// </summary>
     public partial class MainWindow : Window
     {
+        Renderer renderer;
+        Camera camera = new Camera();
+        RenderObj mesh;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadFile()
+        {
+            var parser = new ObjParser();
+            parser.LoadObj("D:\\University\\7th semester\\АКГ\\fox.obj");
+
+            // Choose the back buffer resolution here
+            WriteableBitmap bmp = new WriteableBitmap((int)image.Width, (int)image.Height,
+                300, 300, PixelFormats.Bgra32, null);
+            renderer = new Renderer(bmp);
+            camera.Position = new Vector3(0, 0f, 40.0f);
+            camera.Target = Vector3.Zero;
+            image.Source = bmp;
+
+            mesh = new RenderObj("Fox", parser.VertexList.Count, parser.FaceList.Count);
+            for (var i = 0; i < parser.VertexList.Count; i++)
+            {
+                mesh.Vertices[i] = parser.VertexList[i].ToVector();
+            }
+
+            mesh.Faces = parser.FaceList.ToArray();
+
+            CompositionTarget.Rendering += UpdateAnimation;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void UpdateAnimation(object sender, object e)
+        {
+            mesh.Rotation = new Vector3(mesh.Rotation.X - 0.01f, mesh.Rotation.Y - 0.01f, mesh.Rotation.Z);
+
+            var x = mesh.Position.X;
+            var range = 30;
+
+            if (mesh.Direction < 0)
+            {
+                if (x <= 0) { mesh.Direction = 1; } else { x--; };
+            }
+            else
+            {
+                if (x >= range) { mesh.Direction = -1; } else { x++; };
+            }
+
+            mesh.Position = new Vector3(x, mesh.Position.Y, mesh.Position.Z);
+            renderer.Clear();
+            renderer.Render(camera, new RenderObj[] { mesh });
         }
     }
 }
